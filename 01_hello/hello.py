6a7c0ac2981c47c1abfb9a7cfe1f8a8b8ba508ec
unknown <jon.j.rinko@gmail.com>
2024-07-10 07:04:12 -0400

    updated

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
 
-#Say Hello! 
+"""
+Author: Jon Rinko
+Purpose: Say hello
+"""
 
 import argparse
 
+
 def get_args():
-    parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n','--name', metavar='name', default='World', help='Name to greet')
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument(
+        "-n", "--name", metavar="name", default="World", help="Name to greet"
+    )
     return parser.parse_args()
 
+
 def main():
-   
+    """main"""
+
     args = get_args()
     name = args.name
-    print('Hello, ' + args.name + '!')
+    print("Hello, " + name + "!")
 
-if __name__ == '__main__':  #dunder name == dunger main
+
+if __name__ == "__main__":  # dunder name == dunger main
     main()
