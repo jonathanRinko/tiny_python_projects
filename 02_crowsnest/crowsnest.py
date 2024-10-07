58e17e1b8d61a8c010cb944728b6506fa0620aad
unknown <jon.j.rinko@gmail.com>
2024-10-06 21:22:02 -0400

    updated crows nest

---
+++ 02_crowsnest/crowsnest.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Crow's Nest"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Crow's Nest -- Choose the correct article",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+
+    parser.add_argument('word', metavar='ward', type=str,help='positional str of what you see')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Alert the captain of the item ahead"""
+
+    args = get_args()
+    word = args.word
+    article = 'an' if word[0].lower() in 'aeiou' else 'a'
+
+
+    print(f'Ahoy, Captain! {article} {word} off the larboard bow!')
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
